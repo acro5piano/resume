d2395522a055521b18d7f291dd12122774c7b200
gosho-kazuya <gosho-kazuya@919.jp>
2017-08-05 15:27:08 +0900

    initial

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  context: __dirname + '/src',
+
+  entry: {
+    javascript: './app.js',
+  },
+
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+
+  devServer: {
+    contentBase: 'dist',
+    port: 3000
+  },
+
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.pug$/,
+        exclude: /node_modules/,
+        loader: 'pug-loader'
+      },
+      {
+        test: /\.postcss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin(
+      {
+        template: './index.pug',
+        filename: 'index.html',
+        inject: false
+      }
+    )
+  ]
+};
+
