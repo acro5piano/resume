189f1a0be1789af1fe631ddd52d15dadbb2cfa88
gosho-kazuya <gosho-kazuya@919.jp>
2017-08-06 15:17:49 +0900

    fix basic info

--- src/css/variables.css.js
+++ src/css/variables.css.js
@@ -2,5 +2,7 @@ module.exports = {
   desktop: 'screen and (min-width: 769px)',
   info: '#40cec0',
   accent: 'hsl(152, 84%, 60%)',
-  large: '18px'
+  normal: '13px',
+  large: '16px',
+  xlarge: '18px'
 }
