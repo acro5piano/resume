c4aa4698a0b18d9f915ec7d166cfa594fcb2fd3b
gosho-kazuya <ketsume0211@gmail.com>
2021-07-27 22:20:58 +0900

    wip: move to next.js

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import 'tailwindcss/tailwind.css'
+import '../style.css'
+
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
